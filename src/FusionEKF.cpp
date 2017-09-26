8b668dae9375d16e470acf97c78512c005ff818d
Yury Kirpichev <ykirpichev@yandex-team.ru>
2017-09-27 01:51:08 +0300

    Clean up code a little bit

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -20,7 +20,6 @@ FusionEKF::FusionEKF() {
   R_laser_ = MatrixXd(2, 2);
   R_radar_ = MatrixXd(3, 3);
   H_laser_ = MatrixXd(2, 4);
-  Hj_ = MatrixXd(3, 4);
 
   //measurement covariance matrix - laser
   R_laser_ <<
@@ -53,6 +52,7 @@ FusionEKF::FusionEKF() {
       0, 1, 0, 1,
       0, 0, 1, 0,
       0, 0, 0, 1;
+  ekf_.H_ = H_laser_;
 }
 
 /**
@@ -147,7 +147,6 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   } else {
     // Laser updates
     ekf_.R_ = R_laser_;
-    ekf_.H_ = H_laser_;
     ekf_.Update(measurement_pack.raw_measurements_);
   }
 
--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -8,16 +8,6 @@ KalmanFilter::KalmanFilter() {}
 
 KalmanFilter::~KalmanFilter() {}
 
-void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
-                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
-  x_ = x_in;
-  P_ = P_in;
-  F_ = F_in;
-  H_ = H_in;
-  R_ = R_in;
-  Q_ = Q_in;
-}
-
 void KalmanFilter::Predict() {
   /**
    * predict the state
@@ -45,8 +35,7 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
    * update the state by using Extended Kalman Filter equations
    */
 
-  const double pi = 3.1415926535897;
-  Tools tools;
+  static const double pi = 3.1415926535897;
   double px = x_(0);
   double py = x_(1);
   double vx = x_(2);
@@ -57,19 +46,20 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
       atan2(py, px),
       (px * vx + py * vy) / sqrt(px * px + py * py);
 
-  MatrixXd Hj = tools.CalculateJacobian(x_);
-
   Eigen::VectorXd y = z - Hx;
   if (y(1) < -pi) {
       y(1) = y(1) + 2 * pi;
   } else if (y(1) > pi) {
       y(1) = y(1) - 2 * pi;
   }
+
+  MatrixXd Hj = Tools().CalculateJacobian(x_);
+
   Eigen::MatrixXd S = Hj * P_ * Hj.transpose() + R_;
   Eigen::MatrixXd K = P_ * Hj.transpose() * S.inverse();
 
   x_ = x_ + K * y;
   long x_size = x_.size();
-  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  static const MatrixXd I = MatrixXd::Identity(x_size, x_size);
   P_ = (I - K * Hj) * P_;
 }
